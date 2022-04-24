d52ef59f58d3d3c8301558bceb55f127c707ac73
Emo Enderman <emo@endr.nl>
2022-04-24 21:56:13 +0200

    Removed files because they were not moved for some reason

--- watchpoint.h
+++
@@ -1,6 +0,0 @@
-int watchpoint_init();
-int watchpoint_fini();
-/* The handler function takes the address of the triggered
- * watchpoint, the old value, new value, and user_data. */
-int watchpoint_add(void *addr, int (*handler)(void*, void*, void*, void*), void *user_data);
-int watchpoint_rem(void *addr);
--- watchpointalloc.h
+++
@@ -1,4 +0,0 @@
-int wpa_init();
-int wpa_fini();
-void *wpalloc(size_t size);
-void wpfree(void *ptr);
